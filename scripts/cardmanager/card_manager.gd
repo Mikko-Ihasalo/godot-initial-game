da7a44821c791d6a5e931cb95f8e8531447d64cf
Mikko <mikko.ihasalo@gmail.com>
2026-06-07 16:38:00 +0300

    Implemented a player hand feature

--- scripts/cardmanager/card_manager.gd
+++ scripts/cardmanager/card_manager.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 var screen_size
 var card_being_dragged 
 var is_hovering_on_card
+var player_hand_reference
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -13,7 +15,8 @@ func _input(event: InputEvent) -> void:
 			if card: 
 				start_drag(card)
 		else: 
-			finish_drag()
+			if card_being_dragged:
+				finish_drag()
 			#stuff
 			
 # Called when the node enters the scene tree for the first time.
@@ -29,6 +32,7 @@ func raycast_check_for_card():
 	return null
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	player_hand_reference = $"../playerHand"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -83,6 +87,24 @@ func start_drag(card):
 	card.scale = Vector2(1,1)
 func finish_drag():
 	card_being_dragged.scale = Vector2(1.05,1.05)
+	var card_slot_found = raycast_check_for_cardslot()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		player_hand_reference.remove_card_from_hand(card_being_dragged)
+		card_being_dragged.position = card_slot_found.position
+		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
+	else: 
+		player_hand_reference.add_card_to_hand(card_being_dragged)
 	card_being_dragged = null
 			
+func raycast_check_for_cardslot():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
 	
